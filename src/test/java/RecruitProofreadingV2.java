a2a9a49ad17cb10d1e7020e3a100056e8e33690b
umjammer <umjammer@gmail.com>
2020-04-30 14:29:20 +0900

    fetch more data

--- src/test/java/RecruitProofreadingV2.java
+++ src/test/java/RecruitProofreadingV2.java
@@ -7,6 +7,8 @@
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Pattern;
 
 import vavi.util.properties.annotation.Property;
@@ -31,15 +33,16 @@ public class RecruitProofreadingV2 {
     @Property(name = "java.test.recruit.proofreading.apiKey")
     String apiKey;
 
-    @WebScraper(url = "https://api.a3rt.recruit-tech.co.jp/proofreading/v2/typo?apikey={0}&sentence={1}",
+    @WebScraper(url = "https://api.a3rt.recruit-tech.co.jp/proofreading/v2/typo?apikey={0}&sentence={1}&sensitivity={2}",
                 parser = JsonPathParser.class,
                 input = PlainInputHandler.class,
+                isDebug = true,
                 isCollection = false)
     public static class Result {
         @Target
         String resultID;
         @Target
-        String status;
+        int status;
         @Target
         String message;
         @Target
@@ -48,6 +51,30 @@ public class RecruitProofreadingV2 {
         String normalizedSentence;
         @Target
         String checkedSentence;
+        class Alert {
+            int pos;
+            String word;
+            float score;
+            List<String> suggestions;
+            @Override
+            public String toString() {
+                StringBuilder sb = new StringBuilder();
+                sb.append("Alert [pos=");
+                sb.append(pos);
+                sb.append(", word=");
+                sb.append(word);
+                sb.append(", score=");
+                sb.append(score);
+                sb.append(", suggestions=");
+                sb.append(suggestions);
+                sb.append("]");
+                return sb.toString();
+            }
+        }
+        // TODO should be eliminated
+        public static class MyTypeToken extends com.google.gson.reflect.TypeToken<ArrayList<Alert>> { public MyTypeToken() { super(); }};
+        @Target(option = MyTypeToken.class) /* should be "option = Alert.class" */
+        List<Alert> alerts;
         public String toString() {
             StringBuilder sb = new StringBuilder();
             sb.append(resultID);
@@ -61,27 +88,29 @@ public class RecruitProofreadingV2 {
             sb.append(normalizedSentence);
             sb.append(",");
             sb.append(checkedSentence);
+            sb.append(",");
+            sb.append(alerts);
             return sb.toString();
         }
     }
 
-    void test1(String[] args) throws Exception {
-        String text = "薄茶色のシミがあちこちについた掛け布団。座ったら、五分でお尻が序くなってきそうだ。";
-        Result result = WebScraper.Util.scrape(Result.class, apiKey, text).get(0);
+    public void test1(String[] args) throws Exception {
+        String text = "天秤棒を振り回しながら村への帰路に就いた。時には雑木林の中に足を踏み入れ、戯れに周囲の木立を厳に見立て、突く、打つ、払う。";
+        Result result = WebScraper.Util.scrape(Result.class, apiKey, text, "low").get(0);
         System.err.println(result);
     }
 
     int line = 1;
     int count = 0;
-    Pattern pattern = Pattern.compile("[。、「」]");
+    Pattern pattern = Pattern.compile("[。、「」]"); // TODO check
 
     void test2(String[] args) throws Exception {
         Path file = Paths.get(args[0]);
-        Files.lines(file).forEach(l -> {
+        Files.lines(file).map(l -> l.replaceAll("(｜|［＃.+?］|《.+?》)", "")).forEach(l -> {
             try {
                 if (pattern.matcher(l).find()) {
                     System.out.printf("B: %4d: %s\n", line, l);
-                    Result result = WebScraper.Util.scrape(Result.class, apiKey, l).get(0);
+                    Result result = WebScraper.Util.scrape(Result.class, apiKey, l, "low").get(0);
                     System.out.printf("A: %4d: %s\n", line, result.checkedSentence);
 
                     count++;
@@ -105,7 +134,7 @@ public class RecruitProofreadingV2 {
     public static void main(String[] args) throws Exception {
         RecruitProofreadingV2 app = new RecruitProofreadingV2();
         PropsEntity.Util.bind(app);
-        app.test2(args);
+        app.test1(args);
     }
 }
 
